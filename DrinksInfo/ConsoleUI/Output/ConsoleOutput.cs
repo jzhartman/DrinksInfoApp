fa044bdaf5ae4622f1544ba87e50c1f9507bad60
jzhartman <jzhartman@gmail.com>
2026-03-02 23:00:38 -0500

    Added console output to DI

--- DrinksInfo/ConsoleUI/Output/ConsoleOutput.cs
+++ DrinksInfo/ConsoleUI/Output/ConsoleOutput.cs
@@ -1,5 +1,28 @@
-﻿namespace DrinksInfo.ConsoleUI.Output;
+﻿using Spectre.Console;
 
-internal class ConsoleOutput
+namespace DrinksInfo.ConsoleUI.Output;
+
+public class ConsoleOutput
 {
+    public void RenderDrinkDetailKeyOptions()
+    {
+        var table = new Table()
+                        .RoundedBorder()
+                        .BorderColor(Spectre.Console.Color.Blue)
+                        .ShowRowSeparators();
+
+        table.AddColumn("Key");
+        table.AddColumn("Operation");
+
+        table.AddRow("V", "View Drink Image");
+        table.AddRow("D", "Return to Drink Selection");
+        table.AddRow("C", "Return to Category Selection");
+
+        AnsiConsole.Write(table);
+    }
+
+    public void InputErrorMessage()
+    {
+        AnsiConsole.MarkupLine("[red]ERROR:[/] Invalid key press! Please enter one of the displayed options.");
+    }
 }
