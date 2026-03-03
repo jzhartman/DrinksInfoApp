7df3fb9c6e07a904bb96d97d9d794efb79bdd42d
jzhartman <jzhartman@gmail.com>
2026-03-02 23:01:23 -0500

    Separated Drink Details logic to new service

---
+++ DrinksInfo/ConsoleUI/Services/CategoryListService.cs
@@ -0,0 +1,52 @@
+﻿using DrinksInfo.Application.GetCategories;
+using DrinksInfo.Application.GetDrinksFromCategory;
+using DrinksInfo.ConsoleUI.Views;
+
+namespace DrinksInfo.ConsoleUI.Services;
+
+internal class CategoryListService
+{
+    private readonly CategoryListSelectionView _categorySelection;
+    private readonly DrinkListSelectionView _drinkListSelection;
+    private readonly DrinkDetailService _drinkDetailService;
+    private readonly GetCategoriesHandler _getCategoriesHandler;
+    private readonly GetDrinksSummaryByCategoryNameHandler _getDrinksHandler;
+
+    public CategoryListService(CategoryListSelectionView categorySelection, DrinkListSelectionView drinkListSelection,
+                                GetCategoriesHandler getCategoriesHandler, GetDrinksSummaryByCategoryNameHandler getDrinksHandler,
+                                DrinkDetailService drinkDetailService
+                                         )
+    {
+        _categorySelection = categorySelection;
+        _drinkListSelection = drinkListSelection;
+        _getCategoriesHandler = getCategoriesHandler;
+        _getDrinksHandler = getDrinksHandler;
+        _drinkDetailService = drinkDetailService;
+    }
+
+    public async Task Run()
+    {
+        while (true)
+        {
+            var categories = await _getCategoriesHandler.HandleAsync();
+
+            while (true)
+            {
+                Console.Clear();
+                var categorySelection = _categorySelection.Render(categories.ToArray());
+                int selectionIndex = categories.FindIndex(category => category.Name == categorySelection);
+
+                var drinks = await _getDrinksHandler.Handle(categories[selectionIndex].Name);
+
+                bool returnToCategoryMenu = false;
+
+                while (returnToCategoryMenu == false)
+                {
+                    var drinkSelection = _drinkListSelection.Render(categories[selectionIndex].Name, drinks);
+
+                    returnToCategoryMenu = await _drinkDetailService.ManageDrinkDetails(drinkSelection);
+                }
+            }
+        }
+    }
+}
---
+++ DrinksInfo/ConsoleUI/Services/DrinkDetailService.cs
@@ -0,0 +1,74 @@
+﻿using DrinksInfo.Application.GetDrinkDetailsById;
+using DrinksInfo.Application.GetDrinkImage;
+using DrinksInfo.ConsoleUI.Input;
+using DrinksInfo.ConsoleUI.Output;
+using DrinksInfo.ConsoleUI.Views;
+
+namespace DrinksInfo.ConsoleUI.Services;
+
+public class DrinkDetailService
+{
+    private readonly GetDrinkDetailsByIdHandler _getDrinkDetailsHandler;
+    private readonly GetDrinkImageHandler _getDrinkImageHandler;
+    private readonly DrinkDetailsView _drinkDetails;
+    private readonly DrinkImageView _drinkImage;
+    private readonly ConsoleOutput _output;
+    private readonly UserInput _input;
+
+
+    public DrinkDetailService(GetDrinkDetailsByIdHandler getDrinkDetailsHandler, GetDrinkImageHandler getDrinkImageHandler,
+                                DrinkDetailsView drinkDetails, DrinkImageView drinkImage, ConsoleOutput output, UserInput input)
+    {
+        _getDrinkDetailsHandler = getDrinkDetailsHandler;
+        _getDrinkImageHandler = getDrinkImageHandler;
+        _drinkDetails = drinkDetails;
+        _drinkImage = drinkImage;
+        _output = output;
+        _input = input;
+    }
+
+    public async Task<bool> ManageDrinkDetails(int drinkSelection)
+    {
+        bool returnToCategorySelection = false;
+        bool returnToDrinkSelection = false;
+
+        while (returnToDrinkSelection == false)
+        {
+            Console.Clear();
+
+            var drink = await _getDrinkDetailsHandler.HandleAsync(drinkSelection);
+            _drinkDetails.Render(drink);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            _output.RenderDrinkDetailKeyOptions();
+
+            var keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.V:
+                    Console.Clear();
+                    var imageData = await _getDrinkImageHandler.Handle(drink.ImageUrl);
+                    Console.Clear();
+                    _drinkImage.Render(imageData);
+                    _input.PressAnyKeyToContinue();
+                    break;
+                case ConsoleKey.D:
+                    returnToCategorySelection = false;
+                    returnToDrinkSelection = true;
+                    break;
+                case ConsoleKey.C:
+                    returnToCategorySelection = true;
+                    returnToDrinkSelection = true;
+                    return true;
+                default:
+                    _output.InputErrorMessage();
+                    returnToCategorySelection = false;
+                    returnToDrinkSelection = false;
+                    break;
+            }
+        }
+
+        return returnToCategorySelection;
+    }
+}
