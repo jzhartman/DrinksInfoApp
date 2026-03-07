a9fa6eb4bbe889f5d479f9fd98dbdc4f4dfbd4c8
jzhartman <jzhartman@gmail.com>
2026-03-06 22:37:00 -0500

    Completed first draft of FavoriteDrinks repo

---
+++ DrinksInfo/Application/Interfaces/IFavoriteDrinkRepository.cs
@@ -0,0 +1,12 @@
+﻿using DrinksInfo.Domain.Entities;
+using DrinksInfo.Domain.Validation;
+
+namespace DrinksInfo.Application.Interfaces;
+
+public interface IFavoriteDrinkRepository
+{
+    Task<Result> AddAsync(FavoriteDrink drink);
+    Task<Result> DeleteByIdAsync(int id);
+    Task<Result> ExistsByIdAsync(int id);
+    Task<Result<List<FavoriteDrink>>> GetAllAsync();
+}
--- DrinksInfo/Infrastructure/Repositories/FavoriteDrinkRepository.cs
+++ DrinksInfo/Infrastructure/Repositories/FavoriteDrinkRepository.cs
@@ -1,28 +1,101 @@
 ﻿using Dapper;
+using DrinksInfo.Application.Interfaces;
 using DrinksInfo.Domain.Entities;
+using DrinksInfo.Domain.Validation;
 using DrinksInfo.Infrastructure.Interfaces;
+using Microsoft.Data.Sqlite;
 
 namespace DrinksInfo.Infrastructure.Repositories;
 
-public class FavoriteDrinkRepository
+public class FavoriteDrinkRepository : IFavoriteDrinkRepository
 {
     private readonly ISqliteConnectionFactory _connectionFactory;
     public FavoriteDrinkRepository(ISqliteConnectionFactory connectionFactory)
     {
         _connectionFactory = connectionFactory;
     }
+    public async Task<Result> ExistsByIdAsync(int id)
+    {
+        string sql = "Select count(1) from FavoriteDrink where DrinkId = @Id";
 
-    public async Task<List<DrinkSummary>> GetAll()
+        try
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var response = await connection.ExecuteScalarAsync<int>(sql);
+
+            if (response > 0)
+                return Result.Success();
+            else
+                return Result.Failure(Errors.NoRecordById);
+        }
+        catch (SqliteException ex)
+        {
+            return Result.Failure(new Error(ex.ErrorCode.ToString(), ex.Message));
+        }
+    }
+    public async Task<Result<List<FavoriteDrink>>> GetAllAsync()
     {
         string sql = "Select * from FavoriteDrink";
 
-        using var connection = _connectionFactory.CreateConnection();
-        List<DrinkSummary> drinks = connection.Query<DrinkSummary>(sql).ToList();
-        return drinks;
+        try
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var response = await connection.QueryAsync<FavoriteDrink>(sql);
+
+            if (response.Count() == 0)
+                return Result<List<FavoriteDrink>>.Failure(Errors.EmptyDbResponse);
+
+            return Result<List<FavoriteDrink>>.Success(response.ToList());
+        }
+        catch (SqliteException ex)
+        {
+            return Result<List<FavoriteDrink>>.Failure(new Error(ex.ErrorCode.ToString(), ex.Message));
+        }
     }
 
-    public async Task Add()
+    public async Task<Result> AddAsync(FavoriteDrink drink)
     {
+        string sql = "insert into FavoriteDrink (DrinkId, Name, Category) values (@DrinkId, @Name, @Category)";
 
+        try
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            connection.Execute(sql, drink);
+
+            var result = await ExistsByIdAsync(drink.DrinkId);
+
+            if (result.IsSuccess)
+                return Result.Success();
+            else
+                return Result.Failure(Errors.AddFailed);
+        }
+        catch (SqliteException ex)
+        {
+            return Result.Failure(new Error(ex.ErrorCode.ToString(), ex.Message));
+        }
+    }
+
+    public async Task<Result> DeleteByIdAsync(int id)
+    {
+        string sql = $"delete from FavoriteDrink where Id = {id}";
+
+        try
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            connection.Execute(sql);
+
+            var result = await ExistsByIdAsync(id);
+
+            if (result.IsFailure)
+                return Result.Success();
+            else
+                return Result.Failure(Errors.AddFailed);
+        }
+        catch (SqliteException ex)
+        {
+            return Result.Failure(new Error(ex.ErrorCode.ToString(), ex.Message));
+        }
     }
 }
