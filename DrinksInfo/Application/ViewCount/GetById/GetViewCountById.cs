8989f3a78af9012f351a2d4bb24c64ad064b4059
jzhartman <jzhartman@gmail.com>
2026-03-09 23:41:08 -0400

    Use case for getting view count by id

---
+++ DrinksInfo/Application/ViewCount/GetById/GetViewCountById.cs
@@ -0,0 +1,26 @@
+﻿using DrinksInfo.Domain.Validation;
+using DrinksInfo.Infrastructure.Repositories;
+
+namespace DrinksInfo.Application.ViewCount.GetById;
+
+public class GetViewCountById
+{
+    private readonly IDrinkViewCountRepository _viewCountRepo;
+
+    public GetViewCountById(IDrinkViewCountRepository viewCountRepo)
+    {
+        _viewCountRepo = viewCountRepo;
+    }
+
+    public async Task<Result<int>> HandleAsync(int id)
+    {
+        var result = await _viewCountRepo.GetCountByIdAsync(id);
+
+        if (result.IsFailure)
+            return Result<int>.Failure(result.Errors);
+        if (result.Value == null)
+            return Result<int>.Success(1);
+
+        return Result<int>.Success((int)result.Value.ViewCount);
+    }
+}
