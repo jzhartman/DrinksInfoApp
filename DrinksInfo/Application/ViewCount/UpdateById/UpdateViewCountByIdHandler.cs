113fd306e6e22385aebb08595701f90b838a15fa
jzhartman <jzhartman@gmail.com>
2026-03-11 22:58:10 -0400

    Completed Use Case: View Count Update

---
+++ DrinksInfo/Application/ViewCount/UpdateById/UpdateViewCountByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using DrinksInfo.Domain.Validation;
+using DrinksInfo.Infrastructure.Repositories;
+
+namespace DrinksInfo.Application.ViewCount.UpdateById;
+
+public class UpdateViewCountByIdHandler
+{
+    private readonly IDrinkViewCountRepository _viewCountRepo;
+    public UpdateViewCountByIdHandler(IDrinkViewCountRepository viewCountRepo)
+    {
+        _viewCountRepo = viewCountRepo;
+    }
+
+    public async Task<Result> HandleAsync(int id)
+    {
+        var result = await _viewCountRepo.UpdateCountByIdAsync(id);
+
+        if (result == null)
+            return Result.Failure(Errors.UpdateFailed);
+
+        return result;
+    }
+}
