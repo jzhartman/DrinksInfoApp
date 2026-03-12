3f81e1fd1ff08fb4c4b777567c9cc6721321f0df
jzhartman <jzhartman@gmail.com>
2026-03-11 22:57:52 -0400

    Completed Use Case: View Count Exists

---
+++ DrinksInfo/Application/ViewCount/ExistsById/ViewCountExistsByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using DrinksInfo.Domain.Validation;
+using DrinksInfo.Infrastructure.Repositories;
+
+namespace DrinksInfo.Application.ViewCount.ExistsById;
+
+public class ViewCountExistsByIdHandler
+{
+    private readonly IDrinkViewCountRepository _viewCountRepository;
+    public ViewCountExistsByIdHandler(IDrinkViewCountRepository viewCountRepository)
+    {
+        _viewCountRepository = viewCountRepository;
+    }
+
+    public async Task<Result> HandleAsync(int id)
+    {
+        return await _viewCountRepository.ExistsByIdAsync(id);
+    }
+}
